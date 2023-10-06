5f3416dc0d068696902e19a54d3f5f0a03fd4c39
Carolina Hoeflich <nhoeflich@griddynamics.com>
2023-10-06 11:36:49 -0600

    scratch-project finished

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['airbnb'],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'react/jsx-filename-extension': [2, { extensions: ['.jsx', '.tsx'] }],
+  },
+};
